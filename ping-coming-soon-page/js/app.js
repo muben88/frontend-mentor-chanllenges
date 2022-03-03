c7b6ae363541365f3d958176a3b52a484e2ac97a
Mustapha Ben <87219978+muben88@users.noreply.github.com>
2022-03-03 16:37:04 +0100

    Add files via upload

---
+++ ping-coming-soon-page/js/app.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('.form');
+const input = document.querySelector('input');
+const btn = document.querySelector('.btn');
+const errMsg = document.querySelector('.errMsg');
+const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (input.value.trim() === '' || !input.value.match(pattern)){
+        errMsg.textContent = 'Please provide a valid email address';
+        btn.classList.add('margin');
+        errMsg.classList.remove('success');
+        errMsg.classList.add('error');
+        form.append(errMsg);
+    } else {
+        errMsg.textContent = 'Thank you! We will notify you when we launch!';
+        btn.classList.add('margin');
+        errMsg.classList.remove('error');
+        errMsg.classList.add('success');
+        form.append(errMsg);
+    }
+})
